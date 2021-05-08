48db3a3269aeba7bfe69d3a1b5dfb32af25145e5
Luciano Ferreira <luciano2023silva@gmail.com>
2021-05-08 15:58:23 -0300

    fixing services/api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'https://podcastr-mcyph5dcm-luciano-ferreira.vercel.app/api'
+    baseURL: 'https://podcastr-mzozv0q8g-luciano-ferreira.vercel.app/api'
 })
