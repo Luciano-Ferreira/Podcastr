154bf1a39a59c75cc20181020ae6b6084a93cb70
Luciano Ferreira <luciano2023silva@gmail.com>
2021-05-08 16:01:11 -0300

    fixing services/api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'https://podcastr-mzozv0q8g-luciano-ferreira.vercel.app/api'
+    baseURL: 'https://podcastr-mauve.vercel.app/api'
 })
